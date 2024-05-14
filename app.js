e82ecf106d9b0998797e39fb953de3992e5ce39e
Ismx <ismael.garcia@gladtolink.es>
2024-05-14 13:57:22 +0200

    cors test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('./config/db');
 const app = express();
 
 //Config
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
